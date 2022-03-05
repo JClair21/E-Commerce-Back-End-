9958bca6af735ca98122d84bbc61d279dcb56480
JClair21 <jessicagoodling@gmail.com>
2022-03-05 07:43:55 -0500

    corrected files on my repo

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,14 +10,47 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataType.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
-  }
+{sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelname: 'product',    
+}
 );
+
+
 
 module.exports = Product;
